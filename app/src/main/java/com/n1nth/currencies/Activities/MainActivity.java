b22bc61b136ecf0b6db85fe0112ed621b965ab5d
N1nth <sphinx.n1nth@gmail.com>
2019-05-07 22:23:49 +0300

    refactoring_2

--- app/src/main/java/com/n1nth/currencies/Activities/MainActivity.java
+++ app/src/main/java/com/n1nth/currencies/Activities/MainActivity.java
@@ -1,30 +1,14 @@
 package com.n1nth.currencies.Activities;
 
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.n1nth.currencies.Data.AsyncDownloader;
-import com.n1nth.currencies.Data.Valute;
-import com.n1nth.currencies.Fragments.DontNetworkFragment;
 import com.n1nth.currencies.Fragments.MainFragment;
-import com.n1nth.currencies.Fragments.ParserExceptionFragment;
-import com.n1nth.currencies.NetworkManager;
-import com.n1nth.currencies.Fragments.ProgressFragment;
 import com.n1nth.currencies.R;
 
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List> {
-    public static final String SERVER_URL = "https://www.cbr.ru/scripts/XML_daily.asp";
-    private LoaderManager mLoaderManager;
+public class MainActivity extends AppCompatActivity {
     private MainFragment mMainFragment;
-    private ProgressFragment mProgressFragment;
-    private DontNetworkFragment mDontNetworkFragment;
-    private ParserExceptionFragment mParserExceptionFragment;
-
-
 
 
     @Override
@@ -33,100 +17,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
 
         mMainFragment = new MainFragment();
-        mProgressFragment = new ProgressFragment();
-        mDontNetworkFragment = new DontNetworkFragment();
-        mParserExceptionFragment = new ParserExceptionFragment();
-
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_container, mProgressFragment)
+                .add(R.id.main_container, mMainFragment)
                 .commit();
 
-
-
-        mLoaderManager = getSupportLoaderManager();
-
     }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-
-//        if (mLoaderManager.getLoader(0) != null) {
-//            mLoaderManager.initLoader(0, null, this);
-//        }
-
-
-
-
-        if (NetworkManager.isNetworkAvailable(this)) {
-            // делаем спокойно запрос
-            mLoaderManager.initLoader(1, null, this);
-
-        } else {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_container, mDontNetworkFragment)
-                    .commit();
-        }
-
-
-    }
-
-
-
-
-
-
-
-
-    @Override
-    public Loader<List> onCreateLoader(int i, Bundle bundle) {
-        return new AsyncDownloader(this, SERVER_URL);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List> loader, List list) {
-
-
-        if (list != null) {
-
-            Valute valuteRUB = new Valute();
-            valuteRUB.setId("R00000");
-            valuteRUB.setNumCode(810);
-            valuteRUB.setCharCode("RUB");
-            valuteRUB.setNominal(1);
-            valuteRUB.setName("Российский рубль");
-            valuteRUB.setValue(1.0000);
-
-            list.add(valuteRUB);
-
-            mMainFragment.setmValutes(list);
-
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_container, mMainFragment)
-                    .commit();
-
-        }
-
-         else {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_container, mParserExceptionFragment)
-                    .commit();
-        }
-
-
-
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List> loader) {
-
-    }
-
-
-
 
 }
--- app/src/main/java/com/n1nth/currencies/Fragments/MainFragment.java
+++ app/src/main/java/com/n1nth/currencies/Fragments/MainFragment.java
@@ -3,6 +3,8 @@ package com.n1nth.currencies.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,23 +12,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.n1nth.currencies.Data.AsyncDownloader;
 import com.n1nth.currencies.Data.Valute;
+import com.n1nth.currencies.NetworkManager;
 import com.n1nth.currencies.R;
 
 import java.util.List;
 
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements LoaderManager.LoaderCallbacks<List> {
+    public static final String SERVER_URL = "https://www.cbr.ru/scripts/XML_daily.asp";
 
     private Spinner mCurrencyFrom;
     private Spinner mCurrencyTo;
     private EditText mCurrencyAmount;
     private TextView mCurrencyResult;
     private Button mConvert;
+    private ProgressBar mProgressBar;
+    private TextView mDontNetworkView;
+    private TextView mErrorView;
     private List mValutes;
 
     private int valuteFrom;
@@ -34,17 +43,11 @@ public class MainFragment extends Fragment {
     private double inputValue;
 
 
-    public void setmValutes(List mValutes) {
-        this.mValutes = mValutes;
-    }
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-    }
 
+    }
 
 
     @Override
@@ -62,74 +65,128 @@ public class MainFragment extends Fragment {
         mCurrencyAmount = (EditText) getView().findViewById(R.id.amount_input);
         mCurrencyResult = (TextView) getView().findViewById(R.id.convert_result);
         mConvert = (Button) getView().findViewById(R.id.convert_btn);
+        mProgressBar = (ProgressBar) getView().findViewById(R.id.progress_bar);
+        mDontNetworkView = (TextView) getView().findViewById(R.id.dont_network);
+        mErrorView = (TextView) getView().findViewById(R.id.parser_exception);
 
 
-        /* ------------------------------------------------------------------------------------------------------ */
-        String[] arrayCharCode = new String[mValutes.size()];
-        for (int i=0; i<mValutes.size(); i++) {
+        LoaderManager mLoaderManager = getLoaderManager();
 
-            arrayCharCode[i] = ((Valute) mValutes.get(i)).getCharCode();
+        if (NetworkManager.isNetworkAvailable(getActivity())) {
+            // делаем спокойно запрос
+            mLoaderManager.initLoader(1, null, this);
+
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+            mDontNetworkView.setVisibility(View.VISIBLE);
         }
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, arrayCharCode);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
 
 
-        mCurrencyFrom.setAdapter(adapter);
-        mCurrencyTo.setAdapter(adapter);
 
 
-        mCurrencyFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                valuteFrom = position;
-//                Valute valuteTmp = (Valute) mValutes.get(valuteFrom);
-//                mCurrencyResult.setText(String.valueOf(valuteTmp.getName()));
+    @Override
+    public Loader<List> onCreateLoader(int i, Bundle bundle) {
+        return new AsyncDownloader(this.getActivity(), SERVER_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List> loader, List list) {
+
+        if (list == null) {
+            mProgressBar.setVisibility(View.GONE);
+            mErrorView.setVisibility(View.VISIBLE);
+        }
+
+
+        else {
+            Valute valuteRUB = new Valute();
+            valuteRUB.setId("R00000");
+            valuteRUB.setNumCode(810);
+            valuteRUB.setCharCode("RUB");
+            valuteRUB.setNominal(1);
+            valuteRUB.setName("Российский рубль");
+            valuteRUB.setValue(1.0000);
+            list.add(valuteRUB);
+
+            mValutes = list;
+
+            mProgressBar.setVisibility(View.GONE);
+            mCurrencyFrom.setVisibility(View.VISIBLE);
+            mCurrencyTo.setVisibility(View.VISIBLE);
+            mCurrencyAmount.setVisibility(View.VISIBLE);
+            mCurrencyResult.setVisibility(View.VISIBLE);
+            mConvert.setVisibility(View.VISIBLE);
+
+
+            /* ------------------------------------------------------------------------------------------------------ */
+            String[] arrayCharCode = new String[mValutes.size()];
+            for (int i = 0; i < mValutes.size(); i++) {
+                arrayCharCode[i] = ((Valute) mValutes.get(i)).getCharCode();
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, arrayCharCode);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        mCurrencyTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                valuteTo = position;
-//                Valute valuteTmp = (Valute) mValutes.get(valuteTo);
-//                mCurrencyResult.setText(String.valueOf(valuteTmp.getName()));
-            }
+            mCurrencyFrom.setAdapter(adapter);
+            mCurrencyTo.setAdapter(adapter);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-
-        /* ------------------------------------------------------------------------------------------------------ */
-        mConvert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputStr = String.valueOf(mCurrencyAmount.getText());
-
-                if (!inputStr.isEmpty()) {
-                    inputValue = Double.parseDouble(inputStr);
-
-                    if (inputValue != 0) {
-                        double result = convert(valuteFrom, valuteTo, inputValue);
-                        mCurrencyResult.setText(String.valueOf(result));
-                    } else Toast.makeText(getActivity(), "Input amount, please!", Toast.LENGTH_SHORT).show();
+            mCurrencyFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    valuteFrom = position;
                 }
-                else {
-                    Toast.makeText(getActivity(), "Input amount, please!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+
+            mCurrencyTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    valuteTo = position;
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+
+
+            /* ------------------------------------------------------------------------------------------------------ */
+            mConvert.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String inputStr = String.valueOf(mCurrencyAmount.getText());
+
+                    if (!inputStr.isEmpty()) {
+                        inputValue = Double.parseDouble(inputStr);
+
+                        if (inputValue != 0) {
+                            double result = convert(valuteFrom, valuteTo, inputValue);
+                            mCurrencyResult.setText(String.valueOf(result));
+                        } else
+                            Toast.makeText(getActivity(), "Input amount, please!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Input amount, please!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
     }
-    /* ------------------------------------------------------------------------------------------------------ */
+
+
+    @Override
+    public void onLoaderReset(Loader<List> loader) {
+
+    }
+
+
+
 
 
     private double convert(int valuteFrom, int valuteTo, double inputValue) {
